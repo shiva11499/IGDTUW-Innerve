9a603fe44cca458d2142eb958a439ae1a75b4df4
shiva11499 <gshivansh37@gmail.com>
2018-10-06 13:26:49 +0530

    animation added

--- app/src/main/java/com/example/android/safeline/MainActivity.java
+++ app/src/main/java/com/example/android/safeline/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.safeline;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Button emergency = (Button) findViewById(R.id.emergency);
+
+       emergency.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent smsIntent = new Intent(android.content.Intent.ACTION_VIEW);
+               smsIntent.setType("vnd.android-dir/mms-sms");
+               smsIntent.putExtra("address", "your desired phoneNumber");
+               smsIntent.putExtra("sms_body", "your desired message");
+               startActivity(smsIntent);
+
+           }
+       });
+        }
 }
